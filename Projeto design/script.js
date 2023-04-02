ad5492bcfd4258b59060a878445586c18d0819c8
Thiago Duarte <106554492+ThiagoDuart4@users.noreply.github.com>
2023-04-02 11:47:58 -0300

    Add files via upload

---
+++ Projeto design/script.js
@@ -0,0 +1,9 @@
+
+const LP = document.querySelector(".landinPage")
+const section = document.querySelector(".ImgtextLandingPage")
+const section2 = document.querySelector(".TextLandingPage")
+function PageSec() {
+     LP.style.width = "0%"
+     section.style.display = "none"  
+     section2.style.display = "none" 
+}
